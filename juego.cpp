e519731f2f9f0233da9b822226c5de7cba546aac
Jorge Reinoso <jreinosor1@est.ups.edu.ec>
2022-07-18 10:29:18 -0500

    Circunferencia Dibujada

--- juego.cpp
+++ juego.cpp
@@ -6,39 +6,36 @@ Juego::Juego(QWidget *parent)
     , ui(new Ui::Juego)
 {
     ui->setupUi(this);
-    QPoint centro = ui->marco->geometry().center();
-    m_circulo = new Circulo();
-    m_circulo->setX(centro.x());
-    m_circulo->setY(centro.y());
-    // Instanciando la imagen (creando)
     mImagen = new QImage(this->size(),QImage::Format_ARGB32_Premultiplied);
-    // Rellenar la imagen de color blanco
     mImagen->fill(Qt::white);
-    // Instanciar el Painter a partir de la imagen
     mPainter = new QPainter(mImagen);
     mPainter->setRenderHint(QPainter::Antialiasing);
 
+    QPainter painter();
+
+    int x = 0;
+    int y = 0;
+
+    QPen pincel;
+    pincel.setWidth(5);
+    pincel.setColor(Qt::black);
+    pincel.setJoinStyle(Qt::MiterJoin);
+
+    dibujar();
 }
 
 Juego::~Juego()
 {
     delete ui;
 }
+
 void Juego::paintEvent(QPaintEvent *event)
 {
-   /* // Crear el objeto painter. Establecer el formulario como su lienzo
-    QWidget pintor(this);
-
-    // Crear un nuevo pincel
-    QPen pincel1;
-    pincel1.setColor(Qt::yellow);
-    pincel1.setWidth(5);
-    pincel1.setStyle(Qt::DotLine);
-
-    // Dibuar un circulo
-    pintor.drawEllipse(450, 100, 100, 100);
-*/
+    QPainter painter(this);
+    // Dibujar la imagen
+    painter.drawImage(100, 50, *mImagen);
 }
+
 
 void Juego::on_btnArriba_released()
 {
@@ -60,7 +57,10 @@ void Juego::on_btnIzqueirda_released()
 
 void Juego::on_btnDerecha_released()
 {
+
 }
+
+
 void Juego::on_actionConfigraci0n_triggered()
 {
     Configuracion *config = new Configuracion(this);
@@ -75,5 +75,16 @@ void Juego::on_actionConfigraci0n_triggered()
 void Juego::on_actionSalir_triggered()
 {
     this->close();
+}
+
+void Juego::dibujar()
+{
+    QPen pincel;
+    pincel.setColor(Qt::black);
+    pincel.setWidth(3);
+    // Dibujar una linea
+    mPainter->setPen(pincel);
+    mPainter->drawEllipse(20, 20, 100 , 100);
+    update();
 }
 
--- juego.h
+++ juego.h
@@ -5,14 +5,13 @@
 #include <QDebug>
 #include "configuracion.h"
 #include "circulo.h"
-#include <QImage>
+#include <QPoint>
 #include <QPainter>
-#include <QMouseEvent>
-#include <QPaintEvent>
-#include <QInputDialog>
-#include <QColorDialog>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QImage>
+#include <QMainWindow>
+#include <iomanip>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Juego; }
@@ -26,7 +25,7 @@ public:
     Juego(QWidget *parent = nullptr);
     ~Juego();
 protected:
-    void paintEvent(QPaintEvent *event) override;
+    virtual void paintEvent(QPaintEvent *event);
 
 private slots:
     void on_btnArriba_released();
@@ -41,20 +40,11 @@ private slots:
 
     void on_actionSalir_triggered();
 
-
 private:
     Ui::Juego *ui;
     Circulo *m_circulo;
+    void dibujar();
     QImage *mImagen;        // Imagen sobre la que se va a dibujar
-    QPainter *mPainter;     // Painter de la imagen
-    QPoint mInicial;        // Punto incial para dibujar la línea
-    QPoint mFinal;          // Punto final para dibujar la líne
-    QPoint mTempInic;
-    QPoint mtempFinal;
-    bool mPuedeDibujar;     // Determina si debe o no dibujar
-    int mAncho;             // Define el ancho del pincel
-    QColor mColor;          // Define el color del pincel
-    int mNumLineas;         // Cuenta el número de líneas
-    bool mPuedel;
+    QPainter *mPainter;
 };
 #endif // JUEGO_H
